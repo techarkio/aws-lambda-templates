5d3124712ce9518b159e5a94039c5f9a80eab294
Tech Ark <techark.io@gmail.com>
2018-09-14 17:18:06 +0700

    update params key

--- s3_uploader/s3_uploader.py
+++ s3_uploader/s3_uploader.py
@@ -24,11 +24,13 @@ def lambda_handler(event, context):
     bucket = s3.Bucket(settings.BUCKET_NAME)
 
     multipart_string = base64.b64decode(body_data)
-    content_type = params['Content-Type']
+    content_type = params['header']['Content-Type']
     decoded_parts = decoder.MultipartDecoder(multipart_string, content_type)
     for part in decoded_parts.parts:
         filename = utils.get_filename(part.header)
-        key = settings.FOLDER + '/' + filename + '-' + str(uuid.uuid4)
+        filename, file_extension = os.path.splitext(filename)
+        key = settings.FOLDER + '/' + filename + '-' + \
+            str(uuid.uuid4) + '.' + file_extension
         bucket.put_object(Key=key, Body=part.content)
 
     return {
